f0796b35f9111b40d09e43a306ae88e41a04763d
Илья <camypai.13@list.ru>
2018-02-23 01:46:15 +0300

    Create Solution

---
+++ TaskSaber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskSaber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
