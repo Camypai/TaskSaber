d02d49d90c9e107db29eaa18e540cf803cac9bbc
Илья <camypai.13@list.ru>
2018-02-23 01:55:28 +0300

    Add classes ListNode and ListRand

---
+++ TaskSaber/ListNode.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskSaber
+{
+    public class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand;
+        public string Data;
+    }
+}
---
+++ TaskSaber/ListRand.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace TaskSaber
+{
+    public class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream stream)
+        {
+            
+        }
+
+        public void Deserialize(FileStream stream)
+        {
+            
+        }
+    }
+}
